add precision control and fix bugs

model/lars.py
@@ -1,23 +1,116 @@
+# This file is copied from https://lightning-flash.readthedocs.io/en/stable/_modules/flash/core/optimizers/lars.html#LARS
+# under Apache Licience 2.0
+# the credit and and copyright belongs to PyTorchLightning team(below)
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Implemented by @ananyahjha93
+# also found at: https://github.com/gridai-labs/aavae/tree/main/src/optimizers
+# References:
+#     - https://arxiv.org/pdf/1708.03888.pdf
+#     - https://github.com/pytorch/pytorch/blob/master/torch/optim/sgd.py
 import torch
-from torch.optim.optimizer import Optimizer
+from torch.optim.optimizer import Optimizer, required
+
 
 
 class LARS(Optimizer):
-    """
-    LARS (Layer-wise Adaptive Rate Scaling) optimizer for large-batch training.
+    r"""Extends SGD in PyTorch with LARS scaling.
+
+    See the paper `Large batch training of Convolutional Networks <https://arxiv.org/pdf/1708.03888.pdf>`_
+
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate
+        momentum (float, optional): momentum factor (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov (bool, optional): enables Nesterov momentum (default: False)
+        trust_coefficient (float, optional): trust coefficient for computing LR (default: 0.001)
+        eps (float, optional): eps for division denominator (default: 1e-8)
+
+    Example:
+        >>> from torch import nn
+        >>> model = nn.Linear(10, 1)
+        >>> optimizer = LARS(model.parameters(), lr=0.1, momentum=0.9)
+        >>> optimizer.zero_grad()
+        >>> # loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+
+    .. note::
+        The application of momentum in the SGD part is modified according to
+        the PyTorch standards. LARS scaling fits into the equation in the
+        following fashion.
+
+        .. math::
+            \begin{aligned}
+                g_{t+1} & = \text{lars\_lr} * (\beta * p_{t} + g_{t+1}), \\
+                v_{t+1} & = \mu * v_{t} + g_{t+1}, \\
+                p_{t+1} & = p_{t} - \text{lr} * v_{t+1},
+            \end{aligned}
+
+        where :math:`p`, :math:`g`, :math:`v`, :math:`\mu` and :math:`\beta` denote the
+        parameters, gradient, velocity, momentum, and weight decay respectively.
+        The :math:`lars_lr` is defined by Eq. 6 in the paper.
+        The Nesterov version is analogously modified.
+
+    .. warning::
+        Parameters with weight decay set to 0 will automatically be excluded from
+        layer-wise LR scaling. This is to ensure consistency with papers like SimCLR
+        and BYOL.
     """
 
-    def __init__(self, params, lr=1e-3, momentum=0.9, weight_decay=0, eta=0.001, eps=1e-8):
-        """
-        params: Model parameters to optimize
-        lr: Learning rate
-        momentum: Momentum factor (default: 0.9)
-        weight_decay: Weight decay (L2 penalty) factor (default: 0)
-        eta: Trust coefficient for layer-wise learning rate adaptation
-        eps: A small value to prevent division by zero
-        """
-        defaults = dict(lr=lr, momentum=momentum, weight_decay=weight_decay, eta=eta, eps=eps)
-        super(LARS, self).__init__(params, defaults)
+    def __init__(
+        self,
+        params,
+        lr=required,
+        momentum: float = 0,
+        dampening: float = 0,
+        weight_decay: float = 0,
+        nesterov: bool = False,
+        trust_coefficient: float = 0.001,
+        eps: float = 1e-8,
+    ):
+        if lr is not required and lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if momentum < 0.0:
+            raise ValueError(f"Invalid momentum value: {momentum}")
+        if weight_decay < 0.0:
+            raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+
+        defaults = {
+            "lr": lr,
+            "momentum": momentum,
+            "dampening": dampening,
+            "weight_decay": weight_decay,
+            "nesterov": nesterov,
+        }
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+
+        self.eps = eps
+        self.trust_coefficient = trust_coefficient
+
+        super().__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+
+        for group in self.param_groups:
+            group.setdefault("nesterov", False)
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -36,6 +129,8 @@ class LARS(Optimizer):
         for group in self.param_groups:
             weight_decay = group["weight_decay"]
             momentum = group["momentum"]
+            dampening = group["dampening"]
+            nesterov = group["nesterov"]
 
             for p in group["params"]:
                 if p.grad is None:
@@ -48,7 +143,7 @@ class LARS(Optimizer):
                 # lars scaling + weight decay part
                 if weight_decay != 0 and p_norm != 0 and g_norm != 0:
                     lars_lr = p_norm / (g_norm + p_norm * weight_decay + self.eps)
-                    lars_lr *= self.eta
+                    lars_lr *= self.trust_coefficient
 
                     d_p = d_p.add(p, alpha=weight_decay)
                     d_p *= lars_lr
@@ -60,9 +155,10 @@ class LARS(Optimizer):
                         buf = param_state["momentum_buffer"] = torch.clone(d_p).detach()
                     else:
                         buf = param_state["momentum_buffer"]
-                        buf.mul_(momentum).add_(d_p, alpha=1)
-                    d_p = buf
+                        buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
+                    d_p = d_p.add(buf, alpha=momentum) if nesterov else buf
 
                 p.add_(d_p, alpha=-group["lr"])
 
         return loss
+

model/lightning_models.py
@@ -37,7 +37,7 @@ class CLAP(pl.LightningModule):
         elif self.hparams.optim_name == "LARS":
             optimizer = lars.LARS(params=self.backbone.parameters(),
                                   lr=self.hparams.lr,
-                                  eta = self.hparams.eta,
+                                  trust_coefficient = self.hparams.eta,
                                   momentum=self.hparams.momentum,
                                   weight_decay=self.hparams.weight_decay)
         else:
@@ -210,13 +210,28 @@ class LinearClassification(pl.LightningModule):
 def train_clap(model:pl.LightningModule, train_loader: torch.utils.data.DataLoader,
             max_epochs:int,every_n_epochs:int,
             checkpoint_path:str,
-            num_nodes:int=1,gpu_per_node:int=1,strategy:str="auto"):
+            num_nodes:int=1,gpu_per_node:int=1,strategy:str="auto",precision:str="16-true"):
+    '''
     trainer = pl.Trainer(default_root_dir=checkpoint_path,
                          accelerator="gpu",
                          devices=gpu_per_node,
                          num_nodes=num_nodes,
+                         precision=precision,
                          strategy=strategy,
                          max_epochs=max_epochs,
+                         callbacks=[pl.callbacks.ModelCheckpoint(save_weights_only=True,
+                                                                  save_top_k = -1,
+                                                                  save_last = True,
+                                                                  every_n_epochs = every_n_epochs,
+                                                                  dirpath=checkpoint_path,
+                                                                  filename = "CLAP-{epoch:02d}.ckpt"),
+                                    pl.callbacks.LearningRateMonitor('epoch')])
+    '''
+    trainer = pl.Trainer(default_root_dir=checkpoint_path,
+                         accelerator="gpu",
+                         devices=1,
+                         max_epochs=max_epochs,
+                         precision=precision,
                          callbacks=[pl.callbacks.ModelCheckpoint(save_weights_only=True,
                                                                   save_top_k = -1,
                                                                   save_last = True,
@@ -263,13 +278,15 @@ def train_lc(ssl_model:pl.LightningModule,
             linear_model:pl.LightningModule,
             train_loader: torch.utils.data.DataLoader,
             test_loader:torch.utils.data.DataLoader,
+            val_loader:torch.utils.data.DataLoader,
             max_epochs:int,
             every_n_epochs:int,
             checkpoint_path:str,
             mode:str,
             num_nodes:int=1,
             gpu_per_nodes:int=1,
-            strategy:str = "auto"):
+            strategy:str = "auto",
+            precision:str="16-true"):
     # Check whether pretrained model exists. If yes, load it and skip training
     trained_filename = os.path.join(checkpoint_path, 'last.ckpt')
     if os.path.isfile(trained_filename):
@@ -297,6 +314,7 @@ def train_lc(ssl_model:pl.LightningModule,
                          num_nodes=num_nodes,
                          strategy=strategy,
                          max_epochs=max_epochs,
+                         precision=precision,
                          callbacks=[pl.callbacks.ModelCheckpoint(monitor = "val_acc",
                                                                 mode = "max",
                                                                 dirpath=os.path.join(checkpoint_path,"_temp"),
@@ -305,7 +323,7 @@ def train_lc(ssl_model:pl.LightningModule,
         trainer.logger._default_hp_metric = False 
         ssl_model = CLAP.load_from_checkpoint(os.path.join(ssl_ckpt_path,"last.ckpt"))
         linear_model.set_backbone(ssl_model.backbone)
-        trainer.fit(linear_model, train_loader)
+        trainer.fit(linear_model, train_loader,val_loader)
         test_output = trainer.test(linear_model,test_loader)
         subprocess.check_output(["mv " + os.path.join(best_dir,"*") + " " + checkpoint_path], text=True,shell=True)  
         subprocess.check_output(["rm", "-r", os.path.join(temp_dir)], text=True) 
@@ -326,6 +344,7 @@ def train_lc(ssl_model:pl.LightningModule,
                          devices=gpu_per_nodes,
                          num_nodes=num_nodes,
                          strategy=strategy,
+                         precision=precision,
                          max_epochs=max_epochs,
                          callbacks=[pl.callbacks.ModelCheckpoint(save_weights_only=True,
                                                                   save_top_k = -1,
@@ -341,7 +360,7 @@ def train_lc(ssl_model:pl.LightningModule,
             if os.path.isfile(os.path.join(checkpoint_path,"init.ckpt")):
                 linear_model.load_from_customized_checkpoint(os.path.join(checkpoint_path,"init.ckpt"))
             linear_model.set_backbone(ssl_model.backbone)
-            trainer.fit(linear_model, train_loader)
+            trainer.fit(linear_model, train_loader,val_loader)
             test_output = trainer.test(linear_model,test_loader)
             if result["best_training_loss"]> test_output[0]["test_loss"]:
                 subprocess.check_output(["rm", "-r", best_dir], text=True)

model/models.py
@@ -4,7 +4,7 @@ import json
 import torchvision
 
 class BackboneNet(torch.nn.Module):
-    def __init__(self,resnet_type:str="resnet18",use_projection_header=False,backbone_out_dim,proj_out_dim=-1):
+    def __init__(self,resnet_type:str="resnet18",use_projection_header=False,backbone_out_dim=2048,proj_out_dim=-1):
         super().__init__()
         self.model_name = "BackboneNet"
         # this is a sloppy way of recording hyperperameters